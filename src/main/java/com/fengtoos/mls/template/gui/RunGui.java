dc5db5f5c32e11fbd5e46e82237a3472bd480612
fengtoos <zhuofengyuan@zsmls.com>
2020-06-07 19:23:39 +0800

    更改为windows风格

--- src/main/java/com/fengtoos/mls/template/gui/RunGui.java
+++ src/main/java/com/fengtoos/mls/template/gui/RunGui.java
@@ -1,8 +1,21 @@
 package com.fengtoos.mls.template.gui;
 
+import javax.swing.*;
+
 public class RunGui {
 
     public static void main(String[] args) {
+        //更改默认的选择文件的外观为windows的风格
+        if (UIManager.getLookAndFeel().isSupportedLookAndFeel()) {
+            final String platform = UIManager.getSystemLookAndFeelClassName();
+            if (!UIManager.getLookAndFeel().getName().equals(platform)) {
+                try {
+                    UIManager.setLookAndFeel(platform);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        }
         BuildGui gui = new BuildGui();
     }
 }
